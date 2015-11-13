37911f93bba2d51050c4f23549a1448392f5b7b8
vitor-vfm <vitor.vfm@gmail.com>
2015-11-12 18:35:38 -0700

    Start data retrieval program. Query is correctly parsed

---
+++ data_retrieval.py
@@ -0,0 +1,68 @@
+from bsddb3 import db
+import re
+
+class Query():
+    """
+    Represents a query, including all conditions
+
+    The constructor parses a string and constructs lists
+    of pterms, rterms, general terms, and conditions
+    """
+    def __init__(self, q):
+        """
+        Parse a query
+        """
+        self.pterms = []
+        self.rterms = []
+        self.generalterms = []
+        # conditions are a list of tuples (term, operator, value)
+        # e.g.: rscore > 4 -> ('rscore','>','4')
+        self.conditions = []
+
+        # find pterm specifications
+        m = re.findall(r'p:([a-z]+)', q)
+        if m:
+            self.pterms += m
+        q = re.sub(r'p:[a-z]+', '', q)
+
+        # find rterm specifications
+        m = re.findall(r'r:([a-z]+)', q)
+        if m:
+            self.rterms += m
+        q = re.sub(r'r:[a-z]+', '', q)
+
+        # find conditions
+        m = re.findall(r'([a-z]+) ?([<>]) ?([0-9/]+)', q)
+        if m:
+            self.conditions += m
+        q = re.sub(r'[a-z]+ ?[<>] ?[0-9/]+', '', q)
+
+        # find general terms
+        m = re.findall(r'[a-z%]+', q)
+        if m:
+            self.generalterms += m
+
+def processQuery(query):
+    """
+    Get the results in the database specified 
+    by the query object
+    """
+    # TODO: Get data from db according to query object
+    return None
+
+
+
+def interface():
+    """
+    Basic UI to ask for queries
+    """
+    while True:
+        q = input("Please input your query. Input 'q' to exit: ").lower()
+        if q == 'q':
+            break
+        query = Query(q)
+        res = processQuery(query)
+        print(res)
+
+if __name__=='__main__':
+    interface()
